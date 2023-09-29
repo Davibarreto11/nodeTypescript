8a3204eff6c45b81843915d7d3a38136d919c192
Davibarreto11 <daviarturdesa686@gmail.com>
2023-09-29 13:56:27 -0300

    refactor: ajusts in migrate of appointment

--- src/database/migrations/1695957180602-CreateAppointments.ts
+++ src/database/migrations/1695957180602-CreateAppointments.ts
@@ -11,6 +11,7 @@ export default class CreateAppointments1695957180602 implements MigrationInterfa
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',

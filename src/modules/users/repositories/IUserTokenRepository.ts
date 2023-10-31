59e0a08dc0f91d985c283cba437eb21f7f222e0a
Davi Barreto <daviarturdesa686@gmail.com>
2023-10-31 10:42:47 -0300

    feat: resetpasswordservice

--- src/modules/users/repositories/IUserTokenRepository.ts
+++ src/modules/users/repositories/IUserTokenRepository.ts
@@ -2,4 +2,5 @@ import type UserToken from '../infra/typeorm/entities/UserToken'
 
 export default interface IUserTokenRepository {
   generated: (user_id: string) => Promise<UserToken>
+  findByToken: (token: string) => Promise<UserToken | undefined>
 }

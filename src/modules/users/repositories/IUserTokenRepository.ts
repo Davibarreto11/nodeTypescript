cbc8684b9a9082f01bcaf873d663291c17177bf5
Davi Barreto <daviarturdesa686@gmail.com>
2023-10-31 11:55:07 -0300

    feat: userstokenrepository

--- src/modules/users/repositories/IUserTokenRepository.ts
+++ src/modules/users/repositories/IUserTokenRepository.ts
@@ -2,5 +2,5 @@ import type UserToken from '../infra/typeorm/entities/UserToken'
 
 export default interface IUserTokenRepository {
   generated: (user_id: string) => Promise<UserToken>
-  findByToken: (token: string) => Promise<UserToken | undefined>
+  findByToken: (token: string) => Promise<UserToken | null>
 }

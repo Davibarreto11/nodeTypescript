91b69d37c347ec7f3d8123eb288b584102a63648
Davi Barreto <daviarturdesa686@gmail.com>
2023-10-31 09:44:30 -0300

    refactor: sendforgorpasswordemailservice

---
+++ src/modules/users/repositories/IUserTokenRepository.ts
@@ -0,0 +1,5 @@
+import type UserToken from '../infra/typeorm/entities/UserToken'
+
+export default interface IUserTokenRepository {
+  generated: (user_id: string) => Promise<UserToken>
+}
--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,7 +1,10 @@
 import { injectable, inject } from 'tsyringe'
 
+import AppError from '../../../shared/errors/AppError'
+
 import IUsersRepository from '../repositories/IUsersRepository'
 import IMailProvider from '../providers/MailProvider/models/IMailProvider'
+import IUserTokenRepository from '../repositories/IUserTokenRepository'
 
 interface IRequest {
   email: string
@@ -14,10 +17,21 @@ class SendForgotPasswordEmailService {
     private readonly usersRepository: IUsersRepository,
 
     @inject('MailProvider')
-    private readonly mailProvider: IMailProvider
+    private readonly mailProvider: IMailProvider,
+
+    @inject('UserTokenRepository')
+    private readonly usrsTokenRepository: IUserTokenRepository
   ) {}
 
   public async execute ({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new AppError('User does not exists.')
+    }
+
+    await this.usrsTokenRepository.generated(user.id)
+
     this.mailProvider.sendMail(email, 'Pedido de recuperação de senha recebido')
   }
 }

b0ec226ef41fab5a57b9c77e496fbf6aa2eab525
Davi Barreto <daviarturdesa686@gmail.com>
2023-11-06 18:00:33 -0300

    feat: provideravailabilitycontrollers

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,13 +1,21 @@
 import { Router } from 'express'
 
 import ensureAuthenticated from '../../../../../modules/users/infra/http/middlewares/ensureAuthenticated'
+
 import ProvidersController from '../controllers/ProvidersController'
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController'
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController'
 
 const providersRouter = Router()
+
 const providersController = new ProvidersController()
+const providersMonthAvalabilityController = new ProviderMonthAvailabilityController()
+const providersDayAvalabilityController = new ProviderDayAvailabilityController()
 
 providersRouter.use(ensureAuthenticated)
 
 providersRouter.get('/list', providersController.index)
+providersRouter.get('/:provider_id/month-availability', providersMonthAvalabilityController.index)
+providersRouter.get('/:provider_id/day-availability', providersDayAvalabilityController.index)
 
 export default providersRouter

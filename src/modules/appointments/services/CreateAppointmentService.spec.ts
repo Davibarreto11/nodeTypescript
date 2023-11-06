c39ba8a78c7862bebc75770d75bf4b034073bc20
Davi Barreto <daviarturdesa686@gmail.com>
2023-11-06 17:29:00 -0300

    refactor: createappointementservice

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -13,8 +13,12 @@ describe('CreateAppointment', () => {
   })
 
   test('Should be able to create a new appointment', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2023, 12, 6, 12).getDate()
+    })
+
     const appointment = await createAppointment.execute({
-      date: new Date(),
+      date: new Date(2023, 12, 6, 13),
       provider_id: 'provider_id',
       user_id: 'user_id'
     })
@@ -24,7 +28,11 @@ describe('CreateAppointment', () => {
   })
 
   test('Should not be able to create two appointments on the same time', async () => {
-    const appointmentDate = new Date()
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2023, 12, 6, 12).getDate()
+    })
+
+    const appointmentDate = new Date(2023, 12, 6, 12)
 
     await createAppointment.execute({
       date: appointmentDate,
@@ -32,10 +40,60 @@ describe('CreateAppointment', () => {
       user_id: 'user_id'
     })
 
-    expect(createAppointment.execute({
+    await expect(createAppointment.execute({
       date: appointmentDate,
       provider_id: 'provider_id',
       user_id: 'user_id'
     })).rejects.toBeInstanceOf(AppError)
+  })
+
+  test('Should not be able to create an appointments on past date', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2023, 12, 6, 12).getTime()
+    })
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2023, 12, 6, 11),
+        user_id: 'user_id',
+        provider_id: 'provider_id'
+      })
+    ).rejects.toBeInstanceOf(AppError)
+  })
+
+  test('Should not be able to create an appointments with send user as provider', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2023, 12, 6, 12).getDate()
+    })
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2023, 12, 6, 13),
+        user_id: 'user_id',
+        provider_id: 'user_id'
+      })
+    ).rejects.toBeInstanceOf(AppError)
+  })
+
+  test('Should not be able to create an appointments before 8am and after 6pm', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2023, 12, 6, 12).getDate()
+    })
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2023, 12, 7, 7),
+        user_id: 'user_id',
+        provider_id: 'provider_id'
+      })
+    ).rejects.toBeInstanceOf(AppError)
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2023, 12, 7, 19),
+        user_id: 'user_id',
+        provider_id: 'provider_id'
+      })
+    ).rejects.toBeInstanceOf(AppError)
   })
 })
--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,5 +1,5 @@
-import { startOfHour } from 'date-fns'
 import { injectable, inject } from 'tsyringe'
+import { startOfHour, isBefore, getHours } from 'date-fns'
 
 import AppError from '../../../shared/errors/AppError'
 
@@ -20,7 +20,19 @@ class CreateBetService {
   ) {}
 
   public async execute ({ date, provider_id, user_id }: IRequest): Promise<Appointment> {
-    const appointmentDate = startOfHour(new Date())
+    const appointmentDate = startOfHour(date)
+
+    if (isBefore(appointmentDate, Date.now())) {
+      throw new AppError("You can't create an appointment on past date.")
+    }
+
+    if (user_id === provider_id) {
+      throw new AppError("You can't create an appointment with yourself.")
+    }
+
+    if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 18) {
+      throw new AppError('You can only create appointements between 8am and 18pm.')
+    }
 
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate
